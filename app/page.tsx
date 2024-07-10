4c1c2395132bd7aa2d8559d5917b5197fe6f25f8
karlacabanas01 <karla@kranio.io>
2024-07-09 22:30:25 -0400

    🚀 First part of the portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,273 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Card } from "flowbite-react";
+import { FaLinkedin } from "react-icons/fa";
+import { MdContactMail, MdEmail } from "react-icons/md";
+import { FcContacts } from "react-icons/fc";
+import { IoMdContact } from "react-icons/io";
+import { DiGithub } from "react-icons/di";
+import { BsGithub } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header className="bg-gray-900 p-4 sticky top-0 z-50">
+        <nav className="container mx-auto flex justify-between items-center">
+          <ul className="flex space-x-4">
+            <li>
+              <Link
+                href="#about"
+                className="text-white bg-transparent hover:bg-blue-500 px-4 py-3 rounded"
+              >
+                About me
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#skills"
+                className="text-white bg-transparent hover:bg-blue-500 px-4 py-3 rounded"
+              >
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#soft-skills"
+                className="text-white bg-transparent hover:bg-blue-500 px-4 py-3 rounded"
+              >
+                Soft Skills
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#projects"
+                className="text-white bg-transparent hover:bg-blue-500 px-4 py-3 rounded"
+              >
+                Projects
+              </Link>
+            </li>
+          </ul>
+
+          <div className="space-x-4">
+            <a
+              href="#"
+              className="btn bg-blue-500 text-white py-2 px-4 rounded flex items-center justify-center space-x-2"
+              target="_blank"
+            >
+              <i className="fa fa-download"></i>
+              <span>Descargar CV</span>
+            </a>
+          </div>
+        </nav>
+      </header>
+
+      <div className="w-full p-6 flex">
+        <div className="flex flex-col w-1/2 ml-3 mt-8">
+          <h1 className="text-2xl font-bold text-center">ABOUT ME</h1>
+          <h2 className="text-xl">Hi! Im Tu Nombre</h2>
+          <p className="text-lg">
+            My full name is Tu Nombre Completo, I am XX years old and I am very
+            excited to get my first job in a company. I am a responsible,
+            honest, self-taught, and organized young man. Finally, I live in Tu
+            Ciudad.
+          </p>
+        </div>
+        <div className="flex flex-col w-1/2 items-center">
+          <Image
+            width={150}
+            height={150}
+            src="/img/image.jpeg"
+            alt="Tu Nombre"
+            className="rounded-full"
+          />
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* Skills */}
+      <Card className="text-center mt-8 mb-8 ">
+        <h2 className="text-4xl font-bold mb-4">SKILLS</h2>
+        <div className="flex flex-auto mr-2">
+          <div className="flex flex-wrap justify-center gap-4 mr-2">
+            <div className="flex flex-wrap justify-center gap-4 w-1/4 p-2">
+              <div
+                className="skill relative bg-blue-700 p-4 rounded-lg w-72 h-72 flex items-center justify-center"
+                data-skill="HTML"
+              >
+                <div className="skill-content text-center">
+                  <h3 className="text-xl font-bold mb-2">React</h3>
+                  <div className="circular-chart blue mx-auto">
+                    <svg viewBox="0 0 36 36" className="circular-chart">
+                      <circle
+                        className="circle-bg"
+                        cx="18"
+                        cy="18"
+                        r="15.9155"
+                        fill="none"
+                        stroke="#eeeeee"
+                        strokeWidth="2.8"
+                      ></circle>
+                      <circle
+                        className="circle"
+                        cx="18"
+                        cy="18"
+                        r="15.9155"
+                        fill="none"
+                        stroke="#007bff"
+                        strokeWidth="2.8"
+                        strokeDasharray="70, 100"
+                        strokeDashoffset="0"
+                        strokeLinecap="round"
+                        transform="rotate(-90 18 18)"
+                      ></circle>
+                      <text
+                        x="18"
+                        y="20.35"
+                        className="percentage"
+                        fill="#ffffff"
+                        fontSize="5"
+                        textAnchor="middle"
+                      >
+                        70%
+                      </text>
+                    </svg>
+                  </div>
+                  <div className="skill-level mt-2">Intermediate</div>
+                </div>
+              </div>
+            </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <div className="flex flex-wrap justify-center gap-4 w-1/4 p-2">
+              <div
+                className="skill relative bg-blue-700 p-4 rounded-lg w-72 h-72 flex items-center justify-center"
+                data-skill="HTML"
+              >
+                <div className="skill-content text-center">
+                  <h3 className="text-xl font-bold mb-2">React</h3>
+                  <div className="circular-chart blue mx-auto">
+                    <svg viewBox="0 0 36 36" className="circular-chart">
+                      <circle
+                        className="circle-bg"
+                        cx="18"
+                        cy="18"
+                        r="15.9155"
+                        fill="none"
+                        stroke="#eeeeee"
+                        strokeWidth="2.8"
+                      ></circle>
+                      <circle
+                        className="circle"
+                        cx="18"
+                        cy="18"
+                        r="15.9155"
+                        fill="none"
+                        stroke="#007bff"
+                        strokeWidth="2.8"
+                        strokeDasharray="70, 100"
+                        strokeDashoffset="0"
+                        strokeLinecap="round"
+                        transform="rotate(-90 18 18)"
+                      ></circle>
+                      <text
+                        x="18"
+                        y="20.35"
+                        className="percentage"
+                        fill="#ffffff"
+                        fontSize="5"
+                        textAnchor="middle"
+                      >
+                        70%
+                      </text>
+                    </svg>
+                  </div>
+                  <div className="skill-level mt-2">Intermediate</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Card>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/* Soft Skills */}
+      <Card className="text-center mt-8 mb-8">
+        <h2 className="text-3xl font-bold mt-10">SOFT SKILLS</h2>
+        <div className="flex flex-wrap justify-center gap-4 mr-2">
+          <Card className="flex flex-wrap justify-center w-1/4 p-2">
+            <div className="soft-skill">
+              <Image
+                width={150}
+                height={150}
+                src="/img/image.png"
+                alt="Communication"
+                className="mb-2"
+              />
+              <h3 className="text-xl font-bold">Communication</h3>
+            </div>
+          </Card>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <Card className="flex flex-wrap justify-center w-1/4 p-2">
+            <div className="soft-skill">
+              <Image
+                width={150}
+                height={150}
+                src="/img/image.png"
+                alt="Communication"
+                className="mb-2"
+              />
+              <h3 className="text-xl font-bold">Communication</h3>
+            </div>
+          </Card>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <Card className="flex flex-wrap justify-center w-1/4 p-2 mb-10">
+            <div className="soft-skill">
+              <Image
+                width={150}
+                height={150}
+                src="/img/image.png"
+                alt="Communication"
+                className="mb-2"
+              />
+              <h3 className="text-xl font-bold">Communication</h3>
+            </div>
+          </Card>
+        </div>
+      </Card>
+
+      <Card id="projects" className="text-center mt-8 mb-8">
+        <h2 className="text-3xl font-bold mb-4">PROJECTS</h2>
+      </Card>
+
+      <Card className="flex flex-col items-center text-center p-4">
+        <h2 className="text-3xl font-bold mb-4">CONTACT</h2>
+        <div className="flex flex-wrap justify-center gap-4">
+          <Link
+            href="mailto:your.email@example.com"
+            className="flex items-center bg-gray-500 text-white rounded-full px-4 py-2 space-x-2 hover:bg-gray-600"
+          >
+            <MdEmail />
+            <span>Contact</span>
+          </Link>
+
+          <Link
+            href="https://linkedin.com/in/your-profile"
+            className="flex items-center bg-blue-700 text-white rounded-full px-4 py-2 space-x-2 hover:bg-blue-800"
+          >
+            <FaLinkedin />
+            <span>LinkedIn</span>
+          </Link>
+
+          <Link
+            href="https://github.com/your-username"
+            className="flex items-center bg-gray-900 text-white rounded-full px-4 py-2 space-x-2 hover:bg-gray-700"
+          >
+            <BsGithub />
+            <span>GitHub</span>
+          </Link>
+        </div>
+      </Card>
+
+      <footer className="bg-gray-900 text-white text-center p-4 mt-8">
+        <p>&copy; 2024 Tu Nombre. Todos los derechos reservados.</p>
+      </footer>
+    </>
   );
 }
